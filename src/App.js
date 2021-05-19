89f0336799779f54ee5fbce75e0e02afb51d1fa0
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-05-19 17:41:37 -0500

    Add basic validation to input
    Input accepts only ascii letters, lowercase and uppercase.
    Extend tailwindcss with a min-height 50vh property
    Change layout so no flickering when new options appear below
    Instructions messages are clearer now, the page states when the word
    can't be formed using elements and also when the field is empty.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import SearchInput from './components/SearchInput'
 
 function App() {
   return (
-    <div className="App min-h-screen flex justify-center items-center flex-col">
-      <header className="App-header">
-      </header>
-      <SearchInput></SearchInput>
-    </div>
+    <>
+        <div className="App min-h-1/2 text-gray-800 px-2 flex justify-center items-end">
+          <header className="App-header mb-8">
+          <h1 className="text-6xl mb-4">Elemental names</h1>
+          <p>This web app will use chemical elements to form words. What word should we use?</p>
+          </header>
+        </div>
+        <SearchInput></SearchInput>
+    </>
   );
 }
 
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -6,20 +6,28 @@ const SearchInput = () =>{
     const [word, setWord] = useState('')
     const [elements, setElements] = useState([])
 
+    const removeNumbersAndSpecialCharacters = (word) => {
+        return word.replace(/[^a-zA-Z]/g, '')
+    }
+
     const handleChange = (event) => {
-        setWord(event.target.value)
-        if(event.target.value){setElements(elementalForms(event.target.value))}
+        let cleanedWord = removeNumbersAndSpecialCharacters(event.target.value)
+        setWord(cleanedWord)
+        if(cleanedWord){setElements(elementalForms(cleanedWord))}else{setWord('');setElements([])}
         
     }
 
     return (
         <>
-            <div className="mb-4">
-                <input className="h-12 text-2xl text-gray-800 px-4 rounded-md border border-green-600"  value={word} onChange={handleChange} type="text"/>  
+            <div className="mb-4 flex flex-col content-center items-center">
+                <input className="h-12 text-xl text-gray-800 lg:w-1/3 md:w-2/3 xs:w-full px-4 rounded-md border border-green-600 mb-4" 
+                        placeholder="Your word here, only letters"
+                        name="q" 
+                        value={word} 
+                        onChange={handleChange} 
+                        type="text"/>
+                {elements.length>0? elements.map((arrayOfElements)=><ElementContainer arrayOfElements={arrayOfElements}/>): !word.length>0? "Try writing something":"That word can't be formed using elements. Why don't you try something else?"}
             </div>
-            <div>
-                {elements.length>0? elements.map((arrayOfElements)=><ElementContainer arrayOfElements={arrayOfElements}/>): "Try typing something"}
-            </div>  
         </>)
 }
 
