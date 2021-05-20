c70ae9572414362b581ee18eb57e7f70981039ac
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-05-20 15:43:37 -0500

    Add a credit with components with authors'name

---
+++ src/components/Credits.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Credits = () =>{
+    return (
+        <span className="w-44 p-2 fixed bottom-0 left-0 bg-red-400 font-bold transition duration-300 hover:bg-red-500 whitespace-nowrap overflow-hidden text-white text-sm rounded-r-sm">
+            Credits: <a href="https://coffeebytes.dev">Eduardo Zepeda</a> 
+        </span>
+    )
+}
+
+export default Credits
