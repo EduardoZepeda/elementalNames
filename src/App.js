5cbf0995f90c4f0792fc19bcd2a5d242ef8705ff
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-05-20 15:44:31 -0500

    Add credit component to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import SearchInput from './components/SearchInput'
+import Credits from './components/Credits'
 
 function App() {
   return (
     <>
-        <div className="App min-h-1/2 text-gray-800 px-2 flex justify-center items-end">
+        <div className="App min-h-1/2 text-gray-800 px-4 flex justify-center items-end">
           <header className="App-header mb-8">
           <h1 className="text-6xl mb-4">Elemental names</h1>
-          <p>This web app will use chemical elements to form words. What word should we use?</p>
+          <p>This web app will use chemical elements to form words. Just tell us what word should we use.</p>
           </header>
         </div>
         <SearchInput></SearchInput>
+        <Credits></Credits>
     </>
   );
 }
